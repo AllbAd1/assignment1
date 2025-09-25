5552bf909f5a7a418c96091eef85567b89c5247f
gbs <wlrkq25@gmail.com>
2025-09-25 14:23:34 +0900

    2만원 판별식까지 진행

--- bin/assignment2.dart
+++ bin/assignment2.dart
@@ -1,5 +1,19 @@
-import 'package:assignment1/assignment1.dart' as assignment1;
+void main() {
+  // ignore: unused_local_variable
+  List<String> cart = ["티셔츠", "바지", "모자", "티셔츠", "바지"];
 
-void main(List<String> arguments) {
-  print('Hello world: ${assignment1.calculate()}!');
+  Map<String, int> clothes = {'티셔츠': 10000, '바지': 8000, '모자': 4000};
+  print(clothes['티셔츠']);
+
+  int total = 0;
+  int discount = 0;
+
+  for (var item in cart) {
+    total += clothes[item] ?? 0;
+  }
+  print('장바구니에 $total원어치를 담으셨네요');
+  if (total >= 20000) {
+    print('2만원 이상 구매');
+    
+  }
 }
