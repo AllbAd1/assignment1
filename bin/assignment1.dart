9e2c9494f1a1909348abd973995785c92a26610b
gbs <wlrkq25@gmail.com>
2025-09-25 11:09:26 +0900

    문제 1번 해결

--- bin/assignment1.dart
+++ bin/assignment1.dart
@@ -1,5 +1,18 @@
 import 'package:assignment1/assignment1.dart' as assignment1;
 
-void main(List<String> arguments) {
-  print('Hello world: ${assignment1.calculate()}!');
+void main() {
+  int student = 89;
+  print('필수 문제 1번');
+  
+  if (student >= 90){
+    print('이 학생의 점수는 $student 이며, 등급은 A등급 입니다!');
+  }else if (student < 80){
+    print('이 학생의 점수는 $student 이며, 등급은 C등급 입니다!');
+  }
+ if (student < 80){
+ }else if (student < 90){
+   print('이 학생의 점수는 $student 이며, 등급은 B등급 입니다!');
+   
+   print('이 학생의 점수는 $student 이며, 등급은 입니다');
+ }
 }
