b2d9696bca6de34373c7077c575f3e26841b730b
gbs <wlrkq25@gmail.com>
2025-09-25 15:57:04 +0900

    2번문제 정수형으로 교체 및 간단한 readme 작성

--- bin/assignment2.dart
+++ bin/assignment2.dart
@@ -10,11 +10,11 @@ void main() {
   for (var item in cart) {
     total += clothes[item] ?? 0;
   }
-  print('장바구니에 $total원어치를 담으셨네요');
+  print('장바구니에 $total 원어치를 담으셨네요!');
   if (total >= 20000) {
-    int discount = total ~/ 10 ;
-    print('할인금액 : $discount 원');
-    int totaldiscount = total - discount;
-    print('최종적으로 결제하실 금액은 $totaldiscount 입니다');
+    double discount = total / 10;
+    print('할인금액 : ${discount.toInt()} 원');
+    double totaldiscount = total - discount;
+    print('최종적으로 결제하실 금액은 ${totaldiscount.toInt()} 입니다!');
   }
 }
