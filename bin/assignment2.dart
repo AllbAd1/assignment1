3d4b01ee9818139c52e2b6a1e4d0e97d789b7ed4
gbs <wlrkq25@gmail.com>
2025-09-25 14:34:26 +0900

    2번문제 최종

--- bin/assignment2.dart
+++ bin/assignment2.dart
@@ -3,17 +3,18 @@ void main() {
   List<String> cart = ["티셔츠", "바지", "모자", "티셔츠", "바지"];
 
   Map<String, int> clothes = {'티셔츠': 10000, '바지': 8000, '모자': 4000};
-  print(clothes['티셔츠']);
 
   int total = 0;
   int discount = 0;
-
+  print('필수 문제 2번');
   for (var item in cart) {
     total += clothes[item] ?? 0;
   }
   print('장바구니에 $total원어치를 담으셨네요');
   if (total >= 20000) {
-    print('2만원 이상 구매');
-    
+    double discount = total / 100 * 10;
+    print('할인금액 : $discount 원');
+    double totaldiscount = total - discount;
+    print('최종적으로 결제하실 금액은 $totaldiscount 입니다');
   }
 }
