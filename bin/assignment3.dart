7a0d65013b999d95c1e6d8d116e4c36e2718627c
gbs <wlrkq25@gmail.com>
2025-09-26 11:23:15 +0900

    while 문 버그 수정 (무한반복오류 있었음)

--- bin/assignment3.dart
+++ bin/assignment3.dart
@@ -7,15 +7,24 @@ void main() {
   print('당첨 번호 $winnerNumber');
 
   var random = Random();
-  int result = random.nextInt(45) + 1;
+  //int result = random.nextInt(45) + 1;
 
   List<int> myNumber = [];
-  for (int i = 0; i < 6; i++) {
+  /*for (int i = 0; i < 6; i++) {
     var random = Random();
     int result = random.nextInt(45) + 1;
     myNumber.add(result);
-  }
+  }*/
 
-  
+  while (myNumber.length < 6) {
+    int result = random.nextInt(45) + 1;
+    if (!myNumber.contains(result)) {
+      myNumber.add(result);
+    }
+  }
   print('발급한 로또 번호 $myNumber');
+
+  int matchcount = 0;
+  List<int> matchNumber = [];
+
 }
